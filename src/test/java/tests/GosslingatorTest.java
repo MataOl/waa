f75d84c7e4500768c31ecb07440dbcd65374ff7e
MataOl <martinaoleksova@yahoo.com>
2019-06-01 15:00:58 +0200

    GosslingatorTest

--- src/test/java/tests/GosslingatorTest.java
+++ src/test/java/tests/GosslingatorTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import base.TestBase;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -8,65 +9,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.GosslingatorPage;
 
-public class GosslingatorTest {
-    //POVODNE SME MALI NA ZAC. KAZDEHO TESTU ZADEFINOVANE WebDriver driver = new ChromeDriver();
-    WebDriver driver;
-    String actualNumberOfRyans;
+public class GosslingatorTest extends TestBase {
 
-    //refaktoring kodu
+    private GosslingatorPage gossPage;
+
     @Before
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
-        driver = new ChromeDriver();
-        driver.get("http://localhost/gosslingator.php");
+    public void openPage() {
+        driver.get(BASE_URL + "gosslingator.php");
+        gossPage = new GosslingatorPage(driver);
     }
-
-    @After
-    public void tearDown() {
-        driver.close();
-        driver.quit();
-        System.out.println("s panom bohom idem od vas");
-    }
-
 
     @Test
 // TEST PRIDANIE 1 RYANA A OVERENIE TEXTU V POCITADLE RYANOV ("1","RYAN")
     public void itShoulAddOneRyan() {
-//        kliknut na button addRyan
-        WebElement addRyanBtn = driver.findElement(By.id("addRyan"));
-        addRyanBtn.click();
-        actualNumberOfRyans = driver.findElement(By.id("ryanCounter")).getText();
-        String actualRyanDescription = driver.findElement(By.cssSelector("div.ryan-counter h3")).getText();
+        gossPage.addRyan();
 //        overit pocitanie ryanov
-        Assert.assertEquals("1", actualNumberOfRyans);
-//        vypisem si do konzoly aktualny pocet z pocitadla ryanov
-        System.out.println("Number of ryans: " + actualNumberOfRyans);
-//        overit ze je tam text ryan
-        Assert.assertEquals("ryan", actualRyanDescription);
+        Assert.assertEquals("1", gossPage.getRyanCounterNumber());
     }
 
     @Test
 // TEST PRIDANIE 2 RYANOV A OVERENIE TEXTU V POCITADLE RYANOV ("2","RYANS")
     public void itShoulAddTwoRyans() {
-        WebElement addRyanBtn = driver.findElement(By.id("addRyan"));
-//        2.kliknut na tlacidlo pridat
-        addRyanBtn.click();
-        addRyanBtn.click();
-        actualNumberOfRyans = driver.findElement(By.id("ryanCounter")).getText();
-        String actualRyanDescription = driver.findElement(By.cssSelector("div.ryan-counter h3")).getText();
-//        3.overit pocitanie ryanov
-        Assert.assertEquals("2", actualNumberOfRyans);
-//        vypisem si do konzoly aktualny pocet z pocitadla ryanov
-        System.out.println("Number of ryans: " + actualNumberOfRyans);
-//        overit ze je tam text ryans
-        Assert.assertEquals("ryans", actualRyanDescription);
-    }
+        gossPage.addRyan();
+        gossPage.addRyan();
 
-    @Test
-//  CVICNY TEST OTVORENIE WEBSTRANKY
-    public void itShouldOpenWebpage() {
-        driver.get("https://www.martinus.sk/");
+//      overit pocitanie ryanov
+        Assert.assertEquals("2", gossPage.getRyanCounterNumber());
+
+//      overit ze je tam text ryans
+        Assert.assertEquals("ryans", gossPage.getCounterDescription());
     }
 
     @Test
@@ -78,27 +51,29 @@ public class GosslingatorTest {
 
     @Test
 // TEST 50xKLIK NA RYANA A OVERENIE HLASKY "NUMBER OF RYANS IS TOO DAMN HIGH"
-    public void itShoulDisplayWrongNotice() {
-
-        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
+    public void itShoulDisplayWarningMessage() {
         for (int i = 0; i < 50; i++) {
-            addRyanButton.click();
-            String actualNumberOfRyans = driver.findElement(By.id("ryanCounter")).getText();
-            Assert.assertEquals(String.valueOf(i + 1), actualNumberOfRyans);
+            gossPage.addRyan();
+            Assert.assertEquals(String.valueOf(i + 1), gossPage.getRyanCounterNumber());
+
 //            overit sklonovanie pomocou podmienky
 
-            String actualDescription = driver.findElement(By.cssSelector("div.ryan-counter h3")).getText();
             if (i + 1 == 1) {
-                Assert.assertEquals("ryan", actualDescription);
+                Assert.assertEquals("ryan", gossPage.getCounterDescription());
             }
             if (i + 1 >= 2) {
-                Assert.assertEquals("ryans", actualDescription);
+                Assert.assertEquals("ryans", gossPage.getCounterDescription());
             }
+//            overim pocet obrazkov ryana
+            Assert.assertEquals(i + 1, gossPage.getNumberOfRyanImages());
 
+            System.out.println("index i = " + i);
+
+            System.out.println("pocet ryanov = " + gossPage.getRyanCounterNumber());
         }
+
         //alt+J oznacenie kazdeho dalsieho vyskytu, ked som na prvom vyskyte nastavena modrym
 
-        System.out.println(driver.findElement(By.cssSelector("h1.tooManyRyans")).getText());
         Assert.assertEquals("NUMBER OF\n" +
                 "RYANS\n" +
                 "IS TOO DAMN\n" +
@@ -106,15 +81,17 @@ public class GosslingatorTest {
     }
 
     @Test
-    public void itShouldDisplayWarni
-    ngMessageUsingWhileCycle() {
-        WebElement addRyanButton = driver.findElement(By.id("addRyan"));
-        String actualNumberOfRyans = driver.findElement(By.id("ryanCounter")).getText();
+    public void itShouldDisplayWarningMessageUsingWhileCycle() {
         int clicksLimit = 30;
         int clicks = 0;
-        while (!actualNumberOfRyans.equals("50") && clicks <= clicksLimit){
-            addRyanButton.click();
-        actualNumberOfRyans = driver.findElement(By.id("ryanCounter")).getText();
-        clicks++;}
+        while (!gossPage.getRyanCounterNumber().equals("50") && clicks < clicksLimit) {
+            gossPage.addRyan();
+            clicks++;
+        }
+    }
+
+    @Test
+    public void itShouldDisplayNoRyanOnPageOpen() {
+        Assert.assertEquals(0, gossPage.getNumberOfRyanImages());
     }
 }
