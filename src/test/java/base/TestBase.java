638f0b0534d963a80d5bd0a846bd6d3c83b345f7
MataOl <martinaoleksova@yahoo.com>
2019-06-01 14:56:22 +0200

    Test Base - vytvorenie

---
+++ src/test/java/base/TestBase.java
@@ -0,0 +1,24 @@
+package base;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.GosslingatorPage;
+
+public class TestBase {
+    protected WebDriver driver;
+    protected final String BASE_URL = "http://localhost/";
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
+        driver = new ChromeDriver();
+    }
+
+//    @After
+//    public void tearDown() {
+//        driver.close();
+//        driver.quit();
+//    }
+}
