c9c104887366e74c20b6e31fa0ee53729c87b82a
MataOl <martinaoleksova@yahoo.com>
2019-06-01 15:10:55 +0200

    RegistrationTest

--- src/test/java/tests/RegistrationTest.java
+++ src/test/java/tests/RegistrationTest.java
@@ -1,48 +1,49 @@
 package tests;
 
+import base.TestBase;
+import io.codearte.jfairy.Fairy;
+import io.codearte.jfairy.producer.person.Person;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.RegistrationPage;
 
-public class RegistrationTest {
+import java.util.Locale;
 
-    WebDriver driver;
+import static io.codearte.jfairy.Fairy.*;
+import static io.codearte.jfairy.producer.person.PersonProperties.*;
+
+public class RegistrationTest extends TestBase {
+   private RegistrationPage regPage;
 
     @Before
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("http://localhost/registracia.php");
-
-    }
-
-    @After
-    public void tearDown() {
-        driver.close();
-        driver.quit();
-        System.out.println("s panom bohom idem od vas");
+    public void openPage() {
+        driver.get(BASE_URL + "registracia.php");
+        regPage = new RegistrationPage(driver);
     }
 
     @Test
+//    USPESNA REGISTRACIA
     public void itShouldRegisterValidUser() {
-        String email = "martinaoleksova@yahoo.com";
-        String meno = "Martina";
-        String priezvisko = "Oleksova";
-        String heslo = "Anakonda1292";
 
-        driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("name")).sendKeys(meno);
-        driver.findElement(By.name("surname")).sendKeys(priezvisko);
-        driver.findElement(By.name("password")).sendKeys(heslo);
-        driver.findElement(By.name("password-repeat")).sendKeys(heslo);
 
-        driver.findElement(By.name("robot")).click();
-        driver.findElement(By.cssSelector("button.btn.btn-success")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("div.alert-success")).isDisplayed());
-      //  Assert.assertFalse(driver.findElement(By.cssSelector("div.alert-success")).isDisplayed());
+        regPage.sendRegistrationData();
+        regPage.clickRobotBtn();
+        regPage.clickRegistrujSa();
+        Assert.assertTrue(regPage.getSuccessMessage().isDisplayed());
+    }
+
+    @Test
+//    NEUSPESNA REGISTRACIA
+    public void itShouldDisplayErrorMessageWhenInputsAreEmpty() {
+
+        regPage.clickRobotBtn();
+        regPage.clickRegistrujSa();
+        Assert.assertTrue(regPage.getErrorMessage().isDisplayed());
     }
 }
